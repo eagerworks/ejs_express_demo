27e602b1333d224a73633d7606a7999285c8210f
Florencia Carle <flo.carle.00@gmail.com>
2023-09-26 14:02:53 -0300

    feat: add_prisma

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # express_ejs_demo
+
+## Steps to run
+
+- Create local db and add it to .env
+- Migrate db
+- Run index.js `node index.js`
--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-const express = require("express"),
-  app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PrismaClient } = require("./prisma/generated/client");
+
+const app = express();
+const prisma = new PrismaClient();
+
+// Middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //setting view engine to ejs
 app.set("view engine", "ejs");
 
-const users = [
-  { name: "tobi", email: "tobi@learnboost.com" },
-  { name: "loki", email: "loki@learnboost.com" },
-  { name: "jane", email: "jane@learnboost.com" },
-];
-
 //route for index page
-app.get("/", function (req, res) {
-  res.render("index", {
-    users: users,
-    title: "EJS example",
-    header: "Some users",
-  });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+
+    res.render("index", {
+      users: users,
+      title: "EJS example",
+      header: "Some users",
+    });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.listen(3000, function () {
+app.post("/users", async (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).send("All fields are required.");
+  }
+
+  try {
+    // Create a new user in the database using Prisma
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+
+    // Rerender users
+    res.redirect("/users?sucess=true");
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(3000, () => {
   console.log("Server is running on port 3000 ");
 });
